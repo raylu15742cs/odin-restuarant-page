501989199691f52969cec2e6bf4be15190e59a0c
raylu15742cs <raylu15742cs@gmail.com>
2022-06-02 16:37:06 -0700

    add were done

--- src/page-contact.js
+++ src/page-contact.js
@@ -5,4 +5,55 @@ export default function contactdiv() {
     menuorange.classList.remove("orangetime")
     homeorange.classList.remove("orangetime")
     contactorange.classList.add("orangetime")
+    var checkhome = document.getElementById('homediv')
+    if(checkhome != null) {
+        checkhome.parentNode.removeChild(checkhome)
+    }
+    let mainbody = document.getElementById("content");
+    let maincontent = document.createElement("div");
+    maincontent.setAttribute("id","homediv")
+    maincontent.classList.add('contact')
+    maincontent.classList.add('maincontent')
+    let mctitlediv = document.createElement('div')
+    let mctitle = document.createElement('h1');
+    mctitlediv.classList.add('inneritems')
+    mctitle.innerText = "Contact Us";
+    mctitlediv.appendChild(mctitle)
+    maincontent.appendChild(mctitlediv);
+
+    let intro = document.createElement("div");
+    intro.classList.add('inneritems')
+    intro.classList.add('hoursection')
+    let introh = document.createElement('h2');
+    introh.innerText ='Mama Bear'
+    let introp = document.createElement("p")
+    introp.innerHTML = "Chef <br> 555-555-5555 <br> totallyRealEmail@notFake.com"
+    intro.appendChild(introh)
+    intro.appendChild(introp)
+    maincontent.appendChild(intro)
+
+    let hours = document.createElement("div");
+    hours.classList.add('inneritems')
+    hours.classList.add('hoursection')
+    let hoursh = document.createElement('h2');
+    hoursh.innerText ='Papa Bear'
+    let hoursp = document.createElement("p")
+    hoursp.innerHTML = "Chef <br> 555-555-5555 <br> totallyRealEmail@notFake.com"
+    hours.appendChild(hoursh)
+    hours.appendChild(hoursp)
+    maincontent.appendChild(hours)
+
+    let location = document.createElement("div");
+    location.classList.add('inneritems')
+    location.classList.add('hoursection')
+    let locationh = document.createElement('h2');
+    locationh.innerText ='Baby Bear'
+    let locationp = document.createElement("p")
+    locationp.innerHTML = "Chef <br> 555-555-5555 <br> totallyRealEmail@notFake.com"
+    location.appendChild(locationh)
+    location.appendChild(locationp)
+    maincontent.appendChild(location)
+
+
+    mainbody.appendChild(maincontent);
 }
--- src/page-menu.js
+++ src/page-menu.js
@@ -5,4 +5,52 @@ export default function menudiv() {
     menuorange.classList.add("orangetime")
     homeorange.classList.remove("orangetime")
     contactorange.classList.remove("orangetime")
+    var checkhome = document.getElementById('homediv')
+    if(checkhome != null) {
+        checkhome.parentNode.removeChild(checkhome)
+    }
+    let mainbody = document.getElementById("content");
+    let maincontent = document.createElement("div");
+    maincontent.setAttribute("id","homediv")
+    maincontent.classList.add('menu')
+    maincontent.classList.add('maincontent')
+    let mctitlediv = document.createElement('div')
+    let mctitle = document.createElement('h1');
+    mctitlediv.classList.add('inneritems')
+    mctitle.innerText = "Menu";
+    mctitlediv.appendChild(mctitle)
+    maincontent.appendChild(mctitlediv);
+
+    let intro = document.createElement("div");
+    intro.classList.add('inneritems')
+    let introh = document.createElement('h2');
+    introh.innerText ='Beverages'
+    let introp = document.createElement("p")
+    introp.innerHTML = "Honey Tea : $2 <br> Beary Tea: $2"
+    intro.appendChild(introh)
+    intro.appendChild(introp)
+    maincontent.appendChild(intro)
+
+    let hours = document.createElement("div");
+    hours.classList.add('inneritems')
+    let hoursh = document.createElement('h2');
+    hoursh.innerText ='Sides'
+    let hoursp = document.createElement("p")
+    hoursp.innerHTML = "Toast and Jam: $1 <br> Fresh Fruit: $3"
+    hours.appendChild(hoursh)
+    hours.appendChild(hoursp)
+    maincontent.appendChild(hours)
+
+    let location = document.createElement("div");
+    location.classList.add('inneritems')
+    let locationh = document.createElement('h2');
+    locationh.innerText ='Main Dishes'
+    let locationp = document.createElement("p")
+    locationp.innerHTML = "Pancakes: $4 <br> French Toast: $5 <br> Beary Veggie Sandwich: $8 <br> BLT: $6 <br> Bagel and Lox: $8"
+    location.appendChild(locationh)
+    location.appendChild(locationp)
+    maincontent.appendChild(location)
+
+
+    mainbody.appendChild(maincontent);
 }
